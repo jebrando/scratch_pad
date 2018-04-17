b100acae769686462ba1ca4c2e23af939acc061d
Jelani <jelani.brandon@microsoft.com>
2018-04-17 16:51:54 -0700

    update parsing

--- scratch_pad.c
+++ scratch_pad.c
@@ -8,6 +8,34 @@
 #include "azure_c_shared_utility/buffer_.h"
 
 // Reference https://msdn.microsoft.com/en-us/library/windows/desktop/bb540796(v=vs.85).aspx
+/*
+The ASN.1 definition of an X.509 Certificate as defined by RFC 2459 looks like this
+
+Certificate ::= SEQUENCE {
+tbsCertificate      TBSCertificate,
+signatureAlgorithm  AlgorithmIdentifier,
+signature           BIT STRING
+}
+An X.509 Certificate is simply a signed TBSCertificate.
+
+The definition of a TBSCertificate looks like this
+
+TBSCertificate ::= SEQUENCE {
+version                [0] EXPLICIT Version DEFAULT v1(0),
+serialNumber               CertificateSerialNumber,
+signature                  AlgorithmIdentifier,
+issuer                     Name,
+validity                   Validity,
+subject                    Name,
+subjectPublicKeyInfo       SubjectPublicKeyInfo,
+issuerUniqueId         [1] IMPLICIT UniqueIdentifier OPTIONAL,
+-- If present, version shall be v2 or v3
+subjectUniqueId        [2] IMPLICIT UniqueIdentifier OPTIONAL,
+-- If present, version shall be v2 or v3
+extensions             [3] EXPLICIT Extensions OPTIONAL
+-- If present, version shall be v3
+}
+*/
 
 #define ASN1_MARKER         0x30
 #define LENGTH_EXTENTION    0x82
@@ -36,7 +64,7 @@ typedef enum ASN1_TYPE_TAG
     ASN1_OBJECT_ID = 0x6,
     ASN1_UTF8_STRING = 0xC,
     //ASN1_SET = 0x11,
-    ASN1_NUMERICAL_STRING = 0x13,
+    //ASN1_NUMERICAL_STRING = 0x13,
     ASN1_PRINTABLE_STRING = 0x13,
     ASN1_T61_STRING = 0x16,
     ASN1_UTCTIME = 0x17,
@@ -44,6 +72,20 @@ typedef enum ASN1_TYPE_TAG
     ASN1_SEQUENCE = 0x30,
     ASN1_SET = 0x31
 } ASN1_TYPE;
+
+typedef enum TBS_CERTIFICATE_FIELD_TAG
+{
+    FIELD_VERSION,
+    FIELD_SERIAL_NUM,
+    FIELD_SIGNATURE,
+    FIELD_ISSUER,
+    FIELD_VALIDITY,
+    FIELD_SUBJECT,
+    FIELD_SUBJECT_PUBLIC_KEY_INFO,
+    FIELD_ISSUER_UNIQUE_ID,
+    FIELD_SUBJECT_UNIQUE_ID,
+    FIELD_EXTENSIONS
+} TBS_CERTIFICATE_FIELD;
 
 typedef struct TBS_CERT_INFO_TAG
 {
@@ -65,6 +107,31 @@ typedef struct ASN1_OBJECT_TAG
     uint32_t length;
     const unsigned char* value;
 } ASN1_OBJECT;
+
+static const char* CERTIFICATE_PEM =
+"-----BEGIN CERTIFICATE-----""\n"
+"MIID1zCCAr+gAwIBAgIJALcEbK7ClhupMA0GCSqGSIb3DQEBCwUAMIGBMQswCQYD""\n"
+"VQQGEwJVUzELMAkGA1UECAwCV0ExFDASBgNVBAcMC1dvb2RpbnZpbGxlMRIwEAYD""\n"
+"VQQKDAlNaWNyb3NvZnQxDjAMBgNVBAsMBUF6dXJlMREwDwYDVQQDDAhqZWJyYW5k""\n"
+"bzEYMBYGCSqGSIb3DQEJARYJamJAbXMuY29tMB4XDTE4MDQxNDA1NDUyMloXDTE4""\n"
+"MDQyNDA1NDUyMlowgYExCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJXQTEUMBIGA1UE""\n"
+"BwwLV29vZGludmlsbGUxEjAQBgNVBAoMCU1pY3Jvc29mdDEOMAwGA1UECwwFQXp1""\n"
+"cmUxETAPBgNVBAMMCGplYnJhbmRvMRgwFgYJKoZIhvcNAQkBFglqYkBtcy5jb20w""\n"
+"ggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCmHPz2KVQtDP0PfRv76suF""\n"
+"R821mluRcTnvAqN5weU9LGL91J+0QQRw6faEexU0C86Ozo/P4KULCfrShRAeGvzJ""\n"
+"zyUZWau1wc4+oTkQfAIzOz4KOZpBf2imISfOysw0I3Exm9TNRVAmvapjY2mscRrU""\n"
+"hE8H97jUnaGf0kxfW9VRdoT9CVPmjmK3P3SknChHFdcv9nSEJ98bNtklBH9JCCft""\n"
+"/RcO+4zktD96khQ3srC78Cz8nNjZquK1OwK9p3x+BrukekgW+wAXhjvONNLgF2rb""\n"
+"bCQCDYyZhUu6WAjM4II23/uA5Gie1xYQkRN0i779aA5FcS05fCBqNIyrCWtSqsG1""\n"
+"AgMBAAGjUDBOMB0GA1UdDgQWBBSlTWvqdhHR818KM1PPJncyZYj5qjAfBgNVHSME""\n"
+"GDAWgBSlTWvqdhHR818KM1PPJncyZYj5qjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3""\n"
+"DQEBCwUAA4IBAQCHjZaY9i/E33HUyV7RllZ5fFeUdrbmXK6xJE6xmvaWfnTRBVOH""\n"
+"IMwhjxGRgINm0fqRzzugJrVK2zSoh1rbpTr4zYimB4qaShCYh3jRYS7IukrVfzy0""\n"
+"dw/FdQOQ5q/4F3HqfK9wuk/6g3goUfAiVjuytucmUccO1K8iO2KI5jj3obEZyvvE""\n"
+"dUr0c3yD8C7G/659v+fz07Kjoir4P1ZO2r3xj/G6yqtIYpPG1Z2RSkC5SrxeIQvf""\n"
+"RUbvJuT7nkvUk2bxMlWli/x8rVFuJl7eR5T7MyfgUyLhjrzer1Vex7tVsn8aCKeO""\n"
+"9Bb5qBd3DVQWpT20+NQSYVKblkSqrwxBteQZ""\n"
+"-----END CERTIFICATE-----";
 
 #ifdef WIN32
     static const char* TARGET_CERT = "G:\\Enlistment\\scratch_pad\\cert\\rsa_cert.pem";
@@ -221,10 +288,68 @@ static int parse_asn1_object(unsigned char* tbs_info, ASN1_OBJECT* asn1_obj)
 
 static int parse_tbs_cert_info(unsigned char* tbs_info, size_t len, TBS_CERT_INFO* tbs_cert_info)
 {
-    int result;
+    int result = 0;
     size_t curr_idx = 0;
 
-    unsigned char* iterator;
+    TBS_CERTIFICATE_FIELD tbs_field = FIELD_VERSION;
+    unsigned char* iterator = tbs_info;
+    ASN1_OBJECT target_obj;
+
+    while ((iterator < tbs_info+len) && (result == 0))
+    {
+        switch (tbs_field)
+        {
+            case FIELD_VERSION:
+                // Version field
+                if (*iterator == 0xA0)
+                {
+                    iterator++;
+                    if (*iterator == 0x03)
+                    {
+                        iterator++;
+                        if (parse_asn1_object(iterator, &target_obj) != 0)
+                        {
+                            result = __LINE__;
+                        }
+                        else
+                        {
+                            // Validate version
+                            memcpy(&tbs_cert_info->version, target_obj.value, sizeof(uint32_t));
+                            iterator += target_obj.length;
+                        }
+                    }
+                    else
+                    {
+                        result = __LINE__;
+                    }
+                }
+                else
+                {
+                    result = __LINE__;
+                }
+                break;
+            case FIELD_SERIAL_NUM:
+                if (parse_asn1_object(iterator, &target_obj) != 0)
+                {
+                    result = __LINE__;
+                }
+                else
+                {
+                    //memcpy(&tbs_cert_info->serial_num, target_obj.value, sizeof(uint32_t));
+                    iterator += target_obj.length;
+                }
+                break;
+            case FIELD_SIGNATURE:
+            case FIELD_ISSUER:
+            case FIELD_VALIDITY:
+            case FIELD_SUBJECT:
+            case FIELD_SUBJECT_PUBLIC_KEY_INFO:
+            case FIELD_ISSUER_UNIQUE_ID:
+            case FIELD_SUBJECT_UNIQUE_ID:
+            case FIELD_EXTENSIONS:
+                break;
+        }
+    }
 
     // Figure out version
     if (tbs_info[curr_idx] == 0xA0)
@@ -232,12 +357,6 @@ static int parse_tbs_cert_info(unsigned char* tbs_info, size_t len, TBS_CERT_INF
         curr_idx++;
         if (tbs_info[curr_idx] == 0x3) // Length
         {
-            ASN1_OBJECT target_obj;
-            curr_idx++;
-            parse_asn1_object(&tbs_info[curr_idx], &target_obj);
-            // Validate version
-            memcpy(&tbs_cert_info->version, target_obj.value, sizeof(uint32_t));
-            curr_idx += 0x3;
 
             // Serial Number
             parse_asn1_object(&tbs_info[curr_idx], &target_obj);
@@ -308,9 +427,33 @@ static int parse_certificate(const char* filename)
     return result;
 }
 
+static int parse(const char* cert_pem)
+{
+    int result;
+    BUFFER_HANDLE decoded_cert = decode_cert(cert_pem);
+    if (decoded_cert == NULL)
+    {
+        result = __LINE__;
+    }
+    else
+    {
+        unsigned char* cert_buffer = BUFFER_u_char(decoded_cert);
+        size_t cert_buff_len = BUFFER_length(decoded_cert);
+        // Read 
+        TBS_CERT_INFO tbs_cert_info;
+        parse_asn1_data(cert_buffer, cert_buff_len, STATE_INITIAL, &tbs_cert_info);
+
+        //save_data(BINARY_DATA, BUFFER_u_char(decoded_cert), BUFFER_length(decoded_cert));
+        BUFFER_delete(decoded_cert);
+        result = 0;
+    }
+    return result;
+}
+
 int main(void)
 {
     int result;
-    result = parse_certificate(TARGET_CERT);
+    //result = parse_certificate(TARGET_CERT);
+    result = parse(CERTIFICATE_PEM);
     return result;
 }
